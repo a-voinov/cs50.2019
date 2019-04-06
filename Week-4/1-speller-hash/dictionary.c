86b8c3d91f49b3ccad57f768b3d5682fb2b83ce9
a-voinov <alariht@gmail.com>
2019-04-06 14:41:35 +0700

    Speller (hashtable)

---
+++ Week-4/1-speller-hash/dictionary.c
@@ -0,0 +1,125 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#include "dictionary.h"
+
+// Represents number of buckets in a hash table
+#define N 26
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// Represents a hash table
+node *hashtable[N];
+
+// Hashes word to a number between 0 and 25, inclusive, based on its first letter
+unsigned int hash(const char *word)
+{
+    return tolower(word[0]) - 'a';
+}
+
+int w_size = 0;
+
+// Loads dictionary into memory, returning true if successful else false
+bool load(const char *dictionary)
+{
+    // Initialize hash table
+    for (int i = 0; i < N; i++)
+    {
+        hashtable[i] = NULL;
+    }
+
+    // Open dictionary
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        unload();
+        return false;
+    }
+
+    // Buffer for a word
+    char word[LENGTH + 1];
+
+    // Insert words into hash table
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        node *n = calloc(1, sizeof(node));
+        if (n == NULL)
+        {
+            unload();
+            return false;
+        }
+        strcpy(n->word, word);
+
+        int h = hash(word);
+
+        if (hashtable[h] == NULL)
+        {
+            hashtable[h] = n;
+        }
+        else
+        {
+            node *head = hashtable[h];
+            n->next = head;
+            hashtable[h] = n;
+        }
+        w_size++;
+    }
+
+    // Close dictionary
+    fclose(file);
+
+    // Indicate success
+    return true;
+}
+
+// Returns number of words in dictionary if loaded else 0 if not yet loaded
+unsigned int size(void)
+{
+    return w_size;
+}
+
+// Returns true if word is in dictionary else false
+bool check(const char *word)
+{
+    // lookup for word in hashtable
+    int h = hash(word);
+    node *cursor = hashtable[h];
+    while (cursor != NULL)
+    {
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
+    return false;
+}
+
+// Unloads dictionary from memory, returning true if successful else false
+bool unload(void)
+{
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = hashtable[i];
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+        }
+    }
+
+    return true;
+}
